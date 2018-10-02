83d5c8c9aafe98df1b24cbaebfe630159d5861fe
Azis Abdul Bachar <azis.abdul.bachar@gmail.com>
2018-10-03 06:53:36 +0700

    add comment to code

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const app = express()
 
 /*
  * Izinkan CORS
+ * apa itu CORS?
  * https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
  */
 app.use((request, response, next) => {
@@ -19,25 +20,32 @@ app.use((request, response, next) => {
   next();
 })
 
+// set bodyParser sebagai middleware yang akan memparsing body request
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+
+// set static path untuk static file seperti image, CSS file, JS file
 app.use(express.static(path.join(__dirname, 'public')))
 
+// connect ke database
 mongoose.Promise = global.Promise
 mongoose.connect('mongodb://localhost/todo-app-mern-stack', {
   useNewUrlParser: true
 })
 
+// set route
 app.use('/api', route)
 
 app.get('/', (request, response) => {
   return response.end('API Working')
 })
 
+// jika route tidak ditemukan
 app.use((request, response, next) => {
   response.status(404).send('<h2 align=center>Page not found!</h2>')
 })
 
+// start server
 const port = 3001
 app.listen(port, () => {
   console.log(`App Server Listening at ${port}`)
